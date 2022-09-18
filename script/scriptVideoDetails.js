6159d802504bfd35d81db81a3c36fb812901e7bd
Rima Das <rimadas.automationtester@gmail.com>
2022-09-18 14:34:11 -0700

    some last minute changes

--- script/scriptVideoDetails.js
+++ script/scriptVideoDetails.js
@@ -1,5 +1,7 @@
 var musicVideoURL = 'https://theaudiodb.p.rapidapi.com/mvid.php?i=112024';
-
+// function displayVideo1Details(){
+//     document.addEventListener('click', musicVideo)
+// }
 function musicVideos() {
     const options = {
         method: 'GET',
@@ -8,22 +10,19 @@ function musicVideos() {
             'X-RapidAPI-Host': 'theaudiodb.p.rapidapi.com'
         }
     };
-
     fetch(musicVideoURL, options)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-
-            const { mvids } = data;
-            for (var i = 1; i < 24; i++) {
-                const { strTrack } = data.mvids[i];
-                document.getElementById('songLabel' + (i)).innerText = strTrack;
-                // const {strMusicVid} = data.mvids[i];               
-                console.log(strTrack);
-
-            }
-
+            console.log(data)
+            // // const { mvids } = data;
+            // for (var i = 1; i < 24; i++) {
+            //     const { strTrack } = data.mvids[i];
+            //     document.getElementById('songLabel' + (i)).innerText = strTrack;
+            //     // const {strMusicVid} = data.mvids[i];
+            //     console.log(strTrack);
+            // }
         })
     // .catch(err => console.error(err));
 }
